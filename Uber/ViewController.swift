b466a0cc92a6f8809e109a9e9428b4ed9550177a
paulquinnell <poolq1984@me.com>
2019-05-03 21:14:09 -0400

    First Submission

--- Uber/ViewController.swift
+++ Uber/ViewController.swift
@@ -7,14 +7,79 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var topBtn: UIButton!
+    @IBOutlet weak var bottomBtn: UIButton!
+    @IBOutlet weak var riderLabel: UILabel!
+    @IBOutlet weak var driverLabel: UILabel!
+    
+    var signUpMode = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func topTapped(_ sender: Any) {
+        if emailTextField.text == "" || passwordTextField.text == "" {
+            displayAlert(title: "Missing Informtion", message: "You must enter a Email and Password")
+        } else {
+            if let email = emailTextField.text{
+                if let password = passwordTextField.text {
+            if signUpMode {
+                // SIGNUP
+                Auth.auth().createUser(withEmail: email, password: password, completion: {(user, error) in
+                    if error != nil {
+                        self.displayAlert(title: "Error", message: error!.localizedDescription)
+                    } else {
+                        print("Sign Up Success")
+                        self.performSegue(withIdentifier: "riderSegue", sender: nil)
 
+                    }
+                })
+            }else{
+                // LOGIN
+                Auth.auth().signIn(withEmail: email, password: password, completion: {(user, error) in
+                    if error != nil {
+                        self.displayAlert(title: "Error", message: error!.localizedDescription)
+                    } else {
+                        print("Login Success")
+                        self.performSegue(withIdentifier: "riderSegue", sender: nil)
+                    }
+                })
+            }
+                }}
+        }
+    }
+    
+    func displayAlert(title: String, message: String){
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+        if signUpMode {
+            topBtn.setTitle("Log In", for: .normal)
+            bottomBtn.setTitle("Switch to Sign Up", for: .normal)
+            riderLabel.isHidden = true
+            driverLabel.isHidden = true
+            riderDriverSwitch.isHidden = true
+            signUpMode = false
+        } else {
+            topBtn.setTitle("Sign Up", for: .normal)
+            bottomBtn.setTitle("Switch to Login", for: .normal)
+            riderLabel.isHidden = false
+            driverLabel.isHidden = false
+            riderDriverSwitch.isHidden = false
+            signUpMode = true
+        }
+    }
+    
 }
 
